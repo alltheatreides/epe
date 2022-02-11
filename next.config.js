8571ffb47cbc9d7a2d8c89411ca4fc2578cb2e62
alltheatreides <goldschmidt.antoine@gmail.com>
2022-02-11 15:18:53 +0100

    Bugfix: Vercel Deployment Fix

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   images: {
     domains: ['images.ctfassets.net'],
   },
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_KEY: process.env.CONTENTFUL_ACCESS_KEY,
+  },
 }
