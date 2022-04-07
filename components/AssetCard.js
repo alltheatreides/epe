c6b2ae6d6bbd87e2e76f53d1388258458597372a
alltheatreides <goldschmidt.antoine@gmail.com>
2022-04-07 11:33:14 +0200

    Content: Visual fixes and passHref removal

--- components/AssetCard.js
+++ components/AssetCard.js
@@ -5,9 +5,9 @@ export default function AssetCard({ asset }) {
    const { title, slug, categories, featuresImage, thumbnail } = asset.fields
 
    return (
-      <figure className="card relative mb-4">
+      <figure className="card relative mb-4 bg-[#00000073] rounded-xl p-4">
          <div className="featured cursor-pointer">
-            <Link href={`/assets/${categories[0]}/${categories[1]}/${slug}`} passHref="true" >
+            <Link href={`/assets/${categories[0]}/${categories[1]}/${slug}`}  >
                {/* <> */}
                <Image
                   // src={'https:' + thumbnail.fields.file.url}
@@ -20,8 +20,8 @@ export default function AssetCard({ asset }) {
             </Link>
          </div>
          <figcaption className="content relative z-[1] h-min-[480px]">
-            <div className="info">
-               <h4 className='uppercase font-light lg:tracking-[.25rem] text-justify w-full'>{title}</h4>
+            <div className="info mt-2">
+               <h4 className='uppercase font-light lg:tracking-[.25rem] w-full'>{title}</h4>
                {/* <p>{categories.map}</p> */}
             </div>
          </figcaption>
